7f9e31eb0976522068d0cf544b19a6fd82698a23
minpiee <mjkang0715@gmail.com>
2025-06-11 23:53:57 +0900

    feat: 도서 목록 표시하기

---
+++ src/main/java/com/springboot/service/BookServiceImpl.java
@@ -0,0 +1,23 @@
+package com.springboot.service;
+
+import com.springboot.domain.Book;
+import com.springboot.repository.BookRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+  private final BookRepository bookRepository;
+
+  // 생성자가 하나뿐인 경우 @Autowired 생략 가능 (Spring 4.3 이상)
+  public BookServiceImpl(BookRepository bookRepository) {
+    this.bookRepository = bookRepository;
+  }
+
+  @Override
+  public List<Book> getAllBookList() {
+    return bookRepository.getAllBookList();
+  }
+}
